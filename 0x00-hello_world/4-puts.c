ed7a207ad232e84cba4434c3533db25ab6526738
Saxone Victor <saxonevictor@gmail.com>
2023-02-16 10:20:30 -0800

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,14 @@
 
 
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual puzzle"
  * 
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
-
 
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-
 }
