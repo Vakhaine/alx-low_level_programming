343292154545fc208dfaaffcba511c4bdee280d2
Saxone Victor <saxonevictor@gmail.com>
2023-02-16 10:30:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-
 /**
- * main - Prints "Programming is like building a multilingual puzzle"
+ * main - A Program that prints a line with put function
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
