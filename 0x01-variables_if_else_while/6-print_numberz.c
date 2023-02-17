4bd0379544d7ee8368e53af23a4b3ee9d65dcec8
Saxone Victor <saxonevictor@gmail.com>
2023-02-17 08:32:54 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	putchar('\n');
+	return (0);
+}
