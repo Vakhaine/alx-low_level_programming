a526f227e0afdc5bbaceae983ee498df95d29700
Saxone Victor <saxonevictor@gmail.com>
2023-02-17 07:43:53 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-        putchar(c);
+	putchar(c);
 
 	for (c = 'A'; c <= 'Z'; c++)
-        putchar(c);
+	purchar(c);
 
-    putchar('\n');
-    return 0;
+	purchar('\n');
+	return (0);
 }
