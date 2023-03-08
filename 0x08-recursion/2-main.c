f5afb350fe68f47cb2e8348af0f80c49209a936b
Saxone Victor <saxonevictor@gmail.com>
2023-03-08 03:18:30 -0800

    C-Task

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+/*Declare and define the variable type and name*/
+{
+    int n;
+/* assigning a value to n*/
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
